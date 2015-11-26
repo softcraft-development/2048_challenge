f0fa2fdb3da87e03e836a615d61afa3ef208ba40
Craig Walker <github@softcraft.ca>
2015-11-26 10:59:44 -0700

    initialize the board with randomly generated tiles

--- models/board.rb
+++ models/board.rb
@@ -3,11 +3,23 @@ require "row"
 class Board
   ROW_COUNT = 4
   TILE_COUNT = ROW_COUNT * Row::TILE_COUNT
+  INITIAL_TILE_COUNT = 2
   
   def initialize()
     @rows = ROW_COUNT.times.map{|counter| Row.new }
   end
   
+  def initialize_board
+    INITIAL_TILE_COUNT.times do |initial_tile|
+      tile_number = rand(TILE_COUNT)
+      begin 
+        t = tile(tile_number)
+      end until (t.empty?)
+      
+      t.generate_value
+    end
+  end
+
   def tile(tile_number)
     row = @rows[tile_number / ROW_COUNT]
     tile = row.tile(tile_number % ROW_COUNT)
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -3,6 +3,31 @@ require "spec_helper"
 describe Board do
   subject { Board.new }
   
+  describe "#initialize_board" do
+    let(:result) { subject.initialize_board }
+    let(:nonempty_tiles) do
+      result
+      nonempty_tiles = subject.instance_variable_get(:@rows).reduce([]) do |nonempty_for_row, row|
+        row.instance_variable_get(:@tiles).reduce(nonempty_for_row) do |nonempty_for_tile, tile|
+          nonempty_for_tile << tile unless tile.empty?
+          nonempty_for_tile
+        end
+      end
+    end
+    
+    it "fills two tiles with values" do
+      expect(nonempty_tiles.size).to eq(2)
+    end
+    
+    it "sets the nonempty tiles to have the low or high values" do
+      nonempty_tiles.each do |tile|
+        expect(tile.instance_variable_get(:@value)).to satisfy do |value| 
+          value == Tile::HIGH_GENERATED_TILE_VALUE || value == Tile::LOW_GENERATED_TILE_VALUE
+        end
+      end
+    end
+  end
+  
   describe "#tile" do
     let(:result) { subject.tile(absolute_tile_number)}
     
