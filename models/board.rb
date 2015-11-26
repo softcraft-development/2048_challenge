88994dd8a998a87a51f3ba89ad7e0518766f7ce2
Craig Walker <github@softcraft.ca>
2015-11-26 16:22:30 -0700

    flatten cells into a single array in the board
    Going to rework the concept of how cells are stored

--- models/board.rb
+++ models/board.rb
@@ -1,31 +1,22 @@
-require "row"
+require "cell"
 
 class Board
   ROW_COUNT = 4
-  CELL_COUNT = ROW_COUNT * Row::CELL_COUNT
+  COLUMN_COUNT = 4
+  CELL_COUNT = ROW_COUNT * COLUMN_COUNT
   INITIAL_CELL_COUNT = 2
   
   def initialize()
-    @rows = ROW_COUNT.times.map{|counter| Row.new }
-    @all_cells = []
-    @rows.each do |row|
-      @all_cells.concat(row.cells)
-    end
+    @cells = CELL_COUNT.times.map { Cell.new }
   end
   
   def empty_cells
-    @all_cells.select {|cell| cell.empty?}
+    @cells.select {|cell| cell.empty?}
   end
   
   def initialize_board
     empty_cells.sample(INITIAL_CELL_COUNT).each do |cell|
       cell.generate_tile
     end
-  end
-
-  def cell(cell_number)
-    row = @rows[cell_number / ROW_COUNT]
-    cell = row.cell(cell_number % ROW_COUNT)
-    cell
   end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,22 +2,20 @@ require "spec_helper"
 
 describe Board do
   subject { Board.new }
+  let(:cells) { subject.instance_variable_get(:@cells) }
   
   describe "#empty_cells" do
     context "when there are cells with tiles" do
       let(:result) { subject.empty_cells }
       let!(:nonempty_cell) do
-        row_with_nonempty_cell = subject.instance_variable_get(:@rows)[rand(Board::ROW_COUNT)]
-        nonempty_cell = row_with_nonempty_cell.cell(rand(Row::CELL_COUNT))
-        nonempty_cell.generate_tile
-        nonempty_cell
+        cells.sample.tap do |cell|
+          cell.generate_tile
+        end
       end 
       
       it "returns only the cells without tiles" do
-        target_cells = subject.instance_variable_get(:@all_cells)
-        target_cells.delete(nonempty_cell)
-        
-        expect(result).to eq(target_cells)
+        empty_cells = cells.select { |cell| cell != nonempty_cell }
+        expect(result).to eq(empty_cells)
       end
     end
   end
@@ -26,17 +24,12 @@ describe Board do
     let(:result) { subject.initialize_board }
     let(:tiles) do
       result
-      tiles = subject.instance_variable_get(:@rows).reduce([]) do |tiles_for_board, row|
-        row.instance_variable_get(:@cells).reduce(tiles_for_board) do |tiles_for_row, cell|
-          tiles_for_row << cell.tile unless cell.empty?
-          tiles_for_row
-        end
-        tiles_for_board
-      end
+      nonempty_cells = cells.reject{|cell| cell.empty?}
+      nonempty_cells.map{|cell| cell.tile }
     end
     
     it "fills two cells with tiles" do
-      expect(tiles.size).to eq(2)
+      expect(tiles.size).to eq(Board::INITIAL_CELL_COUNT)
     end
     
     it "sets the tiles to have the low or high values" do
@@ -44,23 +37,6 @@ describe Board do
         expect(tile.value).to satisfy do |value| 
           value == Tile::HIGH_GENERATED_VALUE || value == Tile::LOW_GENERATED_VALUE
         end
-      end
-    end
-  end
-  
-  describe "#cell" do
-    let(:result) { subject.cell(absolute_cell_number)}
-    
-    context "when the cell number corresponds to a cell anywhere in the board" do
-      let(:row_number) { rand(Board::ROW_COUNT) }
-      let(:row_cell_number) { rand(Row::CELL_COUNT) }
-      let(:absolute_cell_number) { (row_number * 4) + row_cell_number }
-      
-      it "returns the cell for the row and column" do
-        row = subject.instance_variable_get(:@rows)[row_number]
-        target_cell = row.instance_variable_get(:@cells)[row_cell_number]
-        
-        expect(result).to be(target_cell)
       end
     end
   end
