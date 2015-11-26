24cf63bd0f4d67aef5a8841906e00be9040ad0ec
Craig Walker <github@softcraft.ca>
2015-11-26 16:28:29 -0700

    ensure at least 2 cells in the cell set

--- spec/models/cell_set_spec.rb
+++ spec/models/cell_set_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
 describe CellSet do
-  let(:cells) { rand(10).times.map { Cell.new } }
+  let(:cells) { (rand(10) + 2).times.map { Cell.new } }
   subject { CellSet.new(cells) }
 end
