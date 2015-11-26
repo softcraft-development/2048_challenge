b3db9410befeeb74925cb206b7721fa7601c3830
Craig Walker <github@softcraft.ca>
2015-11-26 10:36:10 -0700

    query for a tile's emptiness

--- models/tile.rb
+++ models/tile.rb
@@ -2,4 +2,8 @@ class Tile
   def initialize
     @value = nil
   end
+  
+  def empty?
+    @value == nil
+  end
 end
--- spec/models/tile_spec.rb
+++ spec/models/tile_spec.rb
@@ -2,4 +2,21 @@ require "spec_helper"
 
 describe Tile do
   subject { Tile.new }
+  
+  describe "#empty?" do
+    let(:result) { subject.empty? }
+    context "when the tile has no value" do
+      before { subject.instance_variable_set(:@value, nil)}
+      it "is true" do
+        expect(result).to eq(true)
+      end
+    end
+
+    context "when the tile has a value" do
+      before { subject.instance_variable_set(:@value, rand(2048))}
+      it "is false" do
+        expect(result).to eq(false)
+      end
+    end
+  end
 end
