88038cc20f39027a6bb9fe25d2901eea169f4959
Craig Walker <github@softcraft.ca>
2015-11-26 20:07:02 -0700

    code documentation

--- models/board.rb
+++ models/board.rb
@@ -2,6 +2,7 @@ require "cell"
 require "direction"
 
 class Board
+  # These are fully customizable; change them to create a different sized grid
   ROW_COUNT = 4
   COLUMN_COUNT = 4
   CELL_COUNT = ROW_COUNT * COLUMN_COUNT
@@ -10,8 +11,13 @@ class Board
   attr_reader :cells
   attr_reader :directions
   def initialize()
+    # The cells are kept in a single flat array
     @cells = CELL_COUNT.times.map { Cell.new }
     
+    # Then we assign them to "cell sets", which are rows of cells
+    # ordered in a particular direction of movement. A "direction"
+    # is a collection of these cell sets, one for each row/column
+    # of the grid.
     left_cells = COLUMN_COUNT.times.map { [] }
     up_cells = ROW_COUNT.times.map { [] }
     
@@ -28,6 +34,10 @@ class Board
     left_cell_sets = left_cells.map {|cells| CellSet.new(cells)}
     up_cell_sets = up_cells.map {|cells| CellSet.new(cells)}
     
+    # The "right" and "down" directions are just the same cell sets but
+    # with the opposite cell orders. This allows us to have single unified
+    # algorithms for the move/move_possible actions that don't depend
+    # on the direction of the movement on the virtualized "grid".
     @directions = {}
     @directions[:left] = Direction.new(left_cell_sets)
     @directions[:right] = @directions[:left].reverse
