41afe01a803054d6820110619844903e60068fcd
Craig Walker <github@softcraft.ca>
2015-11-26 20:08:15 -0700

    removed cell merge action
    Merging is now done within the set, independently of any cell

--- models/cell.rb
+++ models/cell.rb
@@ -18,15 +18,6 @@ class Cell
     return false
   end
   
-  def merge(tile)
-    if @tile == nil
-      @tile = tile
-    else
-      raise "Cannot merge tiles with differing values" unless @tile.value == tile.value
-      @tile.value += tile.value
-    end
-  end
-  
   def mergeable_with?(tile)
     return true if empty?
     return true if self.tile.value == tile.value
