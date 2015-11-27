a78fbfaa87b414dd6a3d374c8862d9a001d37887
Craig Walker <github@softcraft.ca>
2015-11-26 17:16:30 -0700

    query for cell set move possibility

--- models/cell_set.rb
+++ models/cell_set.rb
@@ -6,6 +6,21 @@ class CellSet
     @cells = cells
   end
   
+  def move_possible?
+    prior_cell = nil
+    @cells.any? do |cell|
+      return true if cell.empty?
+      
+      if prior_cell
+        mergeable = prior_cell.mergeable_with?(cell.tile)
+      else
+        mergeable = false
+      end
+      prior_cell = cell
+      mergeable
+    end
+  end
+  
   def reverse
     CellSet.new(self.cells.reverse)
   end
--- spec/models/cell_set_spec.rb
+++ spec/models/cell_set_spec.rb
@@ -4,6 +4,55 @@ describe CellSet do
   let(:cells) { (rand(10) + 2).times.map { Cell.new } }
   subject { CellSet.new(cells) }
   
+  describe "#move_possible?" do
+    let(:result) { subject.move_possible? }
+    context "when there is an empty cell in the set" do
+      let(:cells) { [Cell.new(Tile.new(rand(2048))), Cell.new]}
+      
+      it "is true" do
+        expect(result).to eq(true)
+      end
+    end
+    
+    context "when there are no empty cells in the set" do
+      context "and adjacent cells have a mergeable tiles" do
+        let(:tile_value) { rand(2048) }
+        let(:unmergable_cell) { Cell.new(Tile.new(tile_value + 1)) }
+        let(:first_mergable_cell) { Cell.new(Tile.new(tile_value)) }
+        let(:second_mergable_cell) { Cell.new(Tile.new(tile_value)) }
+        let(:cells) {[ unmergable_cell, first_mergable_cell, second_mergable_cell]}
+        
+        it "is true" do
+          expect(result).to eq(true)
+        end
+      end
+      
+      context "and non-adjacent cells have a mergeable tiles" do
+        let(:tile_value) { rand(2048) }
+        let(:unmergable_cell) { Cell.new(Tile.new(tile_value + 1)) }
+        let(:first_mergable_cell) { Cell.new(Tile.new(tile_value)) }
+        let(:second_mergable_cell) { Cell.new(Tile.new(tile_value)) }
+        let(:cells) {[ first_mergable_cell, unmergable_cell, second_mergable_cell]}
+        
+        it "is false" do
+          expect(result).to eq(false)
+        end
+      end
+      
+      context "and no cells are mergeable" do
+        let(:tile_value) { rand(2048) }
+        let(:first_unmergable_cell) { Cell.new(Tile.new(tile_value)) }
+        let(:second_mergable_cell) { Cell.new(Tile.new(tile_value + 1))}
+        let(:third_mergable_cell) { Cell.new(Tile.new(tile_value + 2)) }
+        let(:cells) {[ first_unmergable_cell, second_mergable_cell, third_mergable_cell]}
+        
+        it "is false" do
+          expect(result).to eq(false)
+        end
+      end
+    end
+  end
+  
   describe "#reverse" do
     let(:result) { subject.reverse}
 
